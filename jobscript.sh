a26d5e53b054c9907ac6bea80ac11c565878e462
Edu <61661507+edu-bio@users.noreply.github.com>
2023-05-24 20:44:30 +0200

    Update jobscript.sh

--- jobscript.sh
+++ jobscript.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#Empezamos el loop
+#Empezamos el loop 
 for a in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
 do
-#if a es igual a 8 descansa 15 segundos
+#if a es igual a 8 descansa 5 segundos
     if [ $a == 8 ]
     then 
     	sleep 5
