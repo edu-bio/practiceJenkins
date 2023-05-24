3f59548fc12f1dbaa481c4a36ce4601793ecef55
Edu <61661507+edu-bio@users.noreply.github.com>
2023-05-24 20:27:59 +0200

    Update jobscript.sh

--- jobscript.sh
+++ jobscript.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-nombre=$1
-curso=$2
 #Empezamos el loop
 for a in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
 do
-	#if a es igual a 8 descansa 15 segundos
+#if a es igual a 8 descansa 15 segundos
     if [ $a == 8 ]
     then 
     	sleep 5
@@ -14,3 +12,18 @@ do
 done
 sleep 5
 echo "Bien. Hemos terminado con el curso de $curso. Enhorabuena $nombre."
+echo "Vamos a ver la película $pelicula"
+if [ $numero%2 -eq 0 ]
+then
+	echo "$numero es par"
+else
+	echo "$numero es impar"
+fi
+if [ $adulto == true ]
+then
+	echo "Puedes acceder a la sección porno"
+else
+	echo "No puedes acceder todavía a la sección porno, róbale a tus padres el password"
+fi
+sleep 10
+echo "...Fin de la ejecución"
